7ac46fbf1da4aaab14f55ef7d12faaf8be08dcb0
thomashaile <gthomashaile@gmail.com>
2020-06-02 14:25:54 +0200

    albums controller

--- api/albums/controllers.js
+++ api/albums/controllers.js
@@ -1,27 +1,69 @@
 const db = require('../db-connection');
 
 const controllers = {
-  getAll: (req, res) => {
+    getAll: (req, res) => {
 
-    const sql = `SELECT * FROM albums`;
+        const sql = `SELECT * FROM albums`;
 
-    db.all(sql, (err, rows) => {
-      if (err) {
-        res.status(400).json({ "error": err.message });
-        return;
-      }
+        db.all(sql, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
 
-      res.json(rows)
-    });
-  },
-  getOne: (req, res) => { },
-  create: (req, res) => {
-    // read row data from body
-  },
-  update: (req, res) => {
-    // read row data from body
-  },
-  delete: (req, res) => { }
+            res.json(rows)
+        });
+    },
+    getOne: (req, res) => {
+        const id = req.params.id;
+        const sql = `SELECT * FROM albums WHERE AlbumId = ${id}`;
+        db.all(sql, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+            res.json(rows)
+        });
+
+    },
+    create: (req, res) => {
+        // read row data from body
+        const newAlbum = req.body;
+        const sql = `INSERT INTO albums (AlbumId,Title,ArtistId)
+        VALUES(${newAlbum.id},'${newAlbum.title}',${newAlbum.artistId})`;
+        db.all(sql, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+            res.json("New album added successfully...")
+        });
+    },
+    update: (req, res) => {
+        // read row data from body
+        const id = req.params.id;
+        const newAlbum = req.body;
+        const sql = `UPDATE albums SET Title ='${newAlbum.title}',ArtistId =${newAlbum.artistId} WHERE AlbumId = ${id}`;
+        db.all(sql, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+            res.json("Album record updated.....")
+        });
+    },
+    delete: (req, res) => {
+        const id = req.params.id;
+        const sql = `DELETE FROM albums WHERE AlbumId = ${id}`;
+        db.all(sql, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+
+            res.json("Album Successfully Deleted")
+        });
+    }
 }
 
 module.exports = controllers;
